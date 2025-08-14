1066646a99ea4cf780e0dda5d087e104c234a7af
Hugo Pare <hugo.pare@solace.com>
2025-08-14 08:47:07 -0400

    Moved fixtures to conftest file

--- tests/sam-test-infrastructure/src/sam_test_infrastructure/mcp_server/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-This package contains the TestMCPServer, a generic, configurable mock MCP server
-for integration testing.
-"""
-from .server import TestMCPServer
-from .fixture import mcp_server_harness
-
-__all__ = ["TestMCPServer", "mcp_server_harness"]
